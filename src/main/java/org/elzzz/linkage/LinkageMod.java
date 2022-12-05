17ec3dbee5e54c72aa4d4a5d95e615fc187c7de3
Artem Elzzz <mreluzium@mail.ru>
2022-12-06 02:57:40 +0500

    Added available players list update on disconnect

--- src/main/java/org/elzzz/linkage/LinkageMod.java
+++ src/main/java/org/elzzz/linkage/LinkageMod.java
@@ -1,7 +1,7 @@
 package org.elzzz.linkage;
 
-import it.unimi.dsi.fastutil.Hash;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.entity.player.PlayerEntity;
 import org.elzzz.linkage.register.ModItems;
 import org.slf4j.Logger;
@@ -12,10 +12,15 @@ import java.util.HashMap;
 public class LinkageMod implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("linkage");
 	public static final String MOD_ID = "linkage";
-	public static HashMap<String, PlayerEntity> availablePlayers = new HashMap<String, PlayerEntity>();
+	public static HashMap<String, PlayerEntity> availablePlayers = new HashMap<>();
+	// TODO Setup networking so client can access server's availablePlayers instance. Now it's not working on dedicated server.
 
 	@Override
 	public void onInitialize() {
 		ModItems.register();
+
+		ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
+			availablePlayers.remove(handler.player.getUuidAsString());
+		});
 	}
 }
