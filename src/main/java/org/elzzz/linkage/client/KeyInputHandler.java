c27be1e67366a6031f9602a65e0fcd2d1d8c098f
Artem Elzzz <mreluzium@mail.ru>
2022-12-07 01:53:37 +0500

    Added a check whether the ring is equipped

--- src/main/java/org/elzzz/linkage/client/KeyInputHandler.java
+++ src/main/java/org/elzzz/linkage/client/KeyInputHandler.java
@@ -1,11 +1,23 @@
 package org.elzzz.linkage.client;
 
+import dev.emi.trinkets.api.TrinketComponent;
+import dev.emi.trinkets.api.TrinketsApi;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
+import org.elzzz.linkage.register.ModItems;
 import org.lwjgl.glfw.GLFW;
+
+import java.util.Optional;
+
+enum GlypHRingEquippedState {
+    NONE,
+    LOCAL, // GLYPH_RING
+    GLOBAL // INTERDIMENSIONAL_GLYPH_RING
+}
 
 public class KeyInputHandler {
     public static final String KEY_CATEGORY_LINKAGE = "key.category.linkage";
@@ -13,12 +25,29 @@ public class KeyInputHandler {
 
     private static KeyBinding openTeleportationScreenKey;
 
-    public static void registerTeleportationScreenKeyInputs() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (openTeleportationScreenKey.wasPressed()) {
-                client.player.sendMessage(Text.literal("Linkage screen opened"), true);
+    private static void handleTeleportationScreenKey(MinecraftClient client) {
+        while (openTeleportationScreenKey.wasPressed()) {
+            Optional<TrinketComponent> playerTrinketComponent = TrinketsApi.getTrinketComponent(client.player);
+            GlypHRingEquippedState state = GlypHRingEquippedState.NONE;
+
+            if (playerTrinketComponent.isPresent()) {
+                if (playerTrinketComponent.get().isEquipped(ModItems.GLYPH_RING))
+                    state = GlypHRingEquippedState.LOCAL;
+                else if (playerTrinketComponent.get().isEquipped(ModItems.INTERDIMENSIONAL_GLYPH_RING))
+                    state = GlypHRingEquippedState.GLOBAL;
             }
-        });
+
+            switch (state) {
+                case NONE -> client.player.sendMessage(Text.translatable("linkage.gui.no_ring_found"), true);
+                case LOCAL -> client.player.sendMessage(Text.literal("GLYPH_RING"), true);
+                case GLOBAL -> client.player.sendMessage(Text.literal("INTERDIMENSIONAL_GLYPH_RING"), true);
+            }
+
+        }
+    }
+
+    public static void registerTeleportationScreenKeyInputs() {
+        ClientTickEvents.END_CLIENT_TICK.register(KeyInputHandler::handleTeleportationScreenKey);
     }
 
     public static void register() {
